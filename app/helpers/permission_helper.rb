2c8bfe0fd8168e6a8fa9bbd3b6e96da7ab184d40
navro <jakob@navrozidis.com>
2016-02-09 16:30:18 +0100

    Hound fix 2

--- app/helpers/permission_helper.rb
+++ app/helpers/permission_helper.rb
@@ -2,7 +2,7 @@ module PermissionHelper
   def permission_confirmation(perm_user: nil)
     if perm_user.present?
       safe_join([t('permission_user.confirm1'),
-                 perm_user.permission
+                 perm_user.permission,
                  t('permission_user.confirm2'),
                  perm_user.user, '?'])
     end
