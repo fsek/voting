e8332e5b0b5ad93795d86f442237129477d163b8
davidwessman <david.wesmn@gmail.com>
2016-02-10 09:49:51 +0100

    Remove Notice image

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -42,26 +42,11 @@ class NoticesController < ApplicationController
     redirect_to notices_url, notice: alert_destroy(Notice)
   end
 
-  # Action to show notice picture
-  def image
-    if @notice.image?
-      if params[:style] == 'original' || params[:style] == 'large' || params[:style] == 'small'
-        send_file(@notice.image.path(params[:style]),
-                  filename: @notice.image_file_name, type: 'image/jpg',
-                  disposition: 'inline', x_sendfile: true)
-      else
-        send_file(@notice.image.path(:large),
-                  filename: @notice.image_file_name, type: 'image/jpg',
-                  disposition: 'inline', x_sendfile: true)
-      end
-    end
-  end
-
   private
 
   def notice_params
     params.require(:notice).permit(:title, :description,
                                    :public, :d_publish, :d_remove,
-                                   :sort, :image)
+                                   :sort)
   end
 end
