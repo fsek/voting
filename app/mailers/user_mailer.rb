af3557c067936b4fe60e68fdbf7984838a8c1ff6
davidwessman <david.wesmn@gmail.com>
2016-03-21 23:37:16 +0100

    User mailer: update email views
    Updating confirmation_instruction and reset_password_instruction

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,36 @@
 # encoding:UTF-8
-class UserMailer < ActionMailer::Base
-  default from: "svarainte@fsektionen.se"
-  
-  def welcome_email(user)
-    @user = user
-    @url = :new_user_session
-    mail(to: @user.email,subject: 'Välkommen till Fsektionen')
+class UserMailer < Devise::Mailer
+  default(template_path: 'devise/mailer')
+  helper :application # gives access to all helpers defined within `application_helper`.
+
+  def confirmation_instructions(record, token, opts = {})
+    set_message_id
+    opts[:subject] = t('user.mailer.subject_confirm', url: PUBLIC_URL)
+    @user = record
+    super
+  end
+
+  def reset_password_instructions(record, token, opts = {})
+    set_message_id
+    opts[:subject] = t('user.mailer.subject_password', url: PUBLIC_URL)
+    @user = record
+    super
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    set_message_id
+    super
+  end
+
+  def password_change(record, opts = {})
+    set_message_id
+    super
+  end
+
+  private
+
+  def set_message_id
+    str = Time.zone.now.to_i.to_s
+    headers['Message-ID'] = "<#{Digest::SHA2.hexdigest(str)}@fsektionen.se>"
   end
 end
