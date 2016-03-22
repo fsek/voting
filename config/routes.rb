eeb07ee8046eab0f30bcbd44d2fae6e634a4bb86
davidwessman <david.wesmn@gmail.com>
2016-03-22 01:09:53 +0100

    SSL endpoint: adding way to confirm LetsEncrypt
    Sets up a static page where cerificate is confirmed by changing an environment
    variable on Heroku.
    The LetsEncrypt script is then run locally to receive certificates which can
    then be added through the Heroku CLI.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Fsek::Application.routes.draw do
 
   get :cookies_information, controller: :static_pages, as: :cookies, path: :cookies
   get :about, controller: :static_pages, path: :om
+  get :lets_encrypt, controller: :static_pages, path: '.well-known/acme-challenge/:key'
 
   # User-related routes
   devise_for :users, skip: [:sessions, :registrations], controllers: {registrations: "registrations"}
