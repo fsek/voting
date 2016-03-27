183b46624adb46cc7855516299222c1a669e6c0b
davidwessman <david.wesmn@gmail.com>
2016-03-27 19:25:30 +0200

    Contact ability: removing public check

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,17 +7,16 @@ class Ability
     # Abilities that everyone get.
     can :read, Document, public: true
     can :read, News
-    can [:mail, :read], Contact, public: true
+    can [:mail, :read], Contact
     can [:new, :create, :read], Faq
-    can [:index, :about, :cookies_information, :lets_encrypt, :terms], :static_pages
+    can [:index, :about, :cookies_information, :terms], :static_pages
 
     # Abilities all signed in users get
     if user.id.present?
       can [:edit, :show, :update,
            :set_card_number, :update_password,
            :update_account], User, id: user.id
-      can [:read, :mail], Contact
-      can :read, Document, public: true
+      can :read, Document
       can [:index], Vote
       can [:new, :create], VotePost
     end
