e1898f2e05775c367eab1881037413969750d557
davidwessman <david.wesmn@gmail.com>
2016-05-15 22:16:48 +0200

    Force SSL with env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,11 @@ Rails.application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = ENV['FORCE_SSL'] || false
+  if ENV.fetch('FORCE_SSL').to_s.downcase == 'true'
+    config.force_ssl = true
+  else
+    config.force_ssl = false
+  end
 
   # Set to :debug to see everything in the log.
   config.log_level = :warn
