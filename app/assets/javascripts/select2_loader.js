d86af34299e6d23eab92adea3e6575d548e31965
David Wessman <david.wesmn@gmail.com>
2017-10-11 09:26:56 +0200

    JS config and Gemfile cleanup
    - Remove .jshintrc, sprockets doesn't allow ES6 yet.
    - Restructure loading of javascript
    - Rename some javascript methods, they were disagreeing with eachother.

--- app/assets/javascripts/select2_loader.js
+++ app/assets/javascripts/select2_loader.js
@@ -12,11 +12,11 @@ function selectTag() {
 };
 
 // Turbolinks 5 fix
-function clear() {
+function clearSelect2() {
   $('.select2-single').select2('destroy');
   $('.select2-tags').select2('destroy');
 };
 
 $(document).on('turbolinks:load', select);
 $(document).on('turbolinks:load', selectTag);
-$(document).on('turbolinks:before-cache', clear);
+$(document).on('turbolinks:before-cache', clearSelect2);
