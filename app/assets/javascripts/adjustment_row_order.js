9e0c4a68a1279d80569f26689793d742c9cac0e7
David Wessman <david.wesmn@gmail.com>
2017-10-11 08:37:13 +0200

    Remove ES6 syntax

--- app/assets/javascripts/adjustment_row_order.js
+++ app/assets/javascripts/adjustment_row_order.js
@@ -1,47 +1,55 @@
-const adjustment = function () {
+jQuery(function() {
   $('td, th', '#adjustments-sortable').each(function() {
-    const cell = $(this);
+    var cell;
+    cell = $(this);
     return cell.width(cell.width());
   });
-
   $('#adjustments-sortable').sortable({
     axis: 'y',
     items: '.item',
     cursor: 'move',
     forcePlaceholderSize: true,
     placeholder: 'must-have-class',
-
-    start(e, ui) {
-      let cellCount = 0;
+    start: function(e, ui) {
+      var cellCount;
+      cellCount = 0;
       $('td, th', ui.helper).each(function() {
-        let colspan = 1;
-        const colspanAttr = $(this).attr('colspan');
+        var colspan, colspanAttr;
+        colspan = 1;
+        colspanAttr = $(this).attr('colspan');
         if (colspanAttr > 1) {
           colspan = colspanAttr;
         }
         return cellCount += colspan;
       });
-      return ui.placeholder.html(`<td colspan="${cellCount}">&nbsp</td>`);
+      return ui.placeholder.html('<td colspan="' + cellCount + '">&nbsp</td>');
     },
-
-    sort(e, ui) {
+    sort: function(e, ui) {
       return ui.item.addClass('active-item-shadow');
     },
-    stop(e, ui) {
+    stop: function(e, ui) {
       return ui.item.removeClass('active-item-shadow');
     },
-    update(e, ui) {
-      const item_id = ui.item.data('item-id');
-      const position = ui.item.index();
+    update: function(e, ui) {
+      var item_id, position;
+      item_id = ui.item.data('item-id');
+      position = ui.item.index();
       return $.ajax({
         type: 'POST',
         url: '/admin/justering/update_row_order',
         dataType: 'json',
-        data: { id: item_id, adjustment: {row_order_position: position } },
-        success() {
-          return ui.item.children('td').effect('highlight', { color: '#fc9800'}, 3000);
+        data: {
+          id: item_id,
+          adjustment: {
+            row_order_position: position
+          }
         },
-        error(response) {
+        success: function() {
+          return ui.item.children('td').effect('highlight', {
+            color: '#fc9800'
+          }, 3000);
+        },
+        error: function(response) {
           jQuery('#adjustments-sortable').sortable('cancel');
           jQuery('#adjustments-sortable').sortable('disable');
           return alert('Sorteringen misslyckades. Ladda om sidan!');
@@ -49,27 +57,27 @@ const adjustment = function () {
       });
     }
   });
-
-  $('.adjustments-deleterow').on('click', function() {
+  return $('.adjustments-deleterow').on('click', function() {
+    var $row;
     if (confirm('Är du helt säker på att du vill ta bort justeringen?')) {
-      const $row = $(this).parent('tr');
+      $row = $(this).parent('tr');
       return $.ajax({
         type: 'POST',
-        url: `/admin/justering/${$row.data('item-id')}`,
+        url: '/admin/justering/' + $row.data('item-id'),
         dataType: 'json',
-        data: { '_method': 'delete' },
-        success() {
+        data: {
+          '_method': 'delete'
+        },
+        success: function() {
           $row.addClass('danger');
           return $row.fadeOut(3000, function() {
             return $(this).remove();
           });
         },
-        error() {
+        error: function() {
           return alert('Det gick inte att ta bort justeringen');
         }
       });
     }
   });
-};
-
-$(document).on('turbolinks:load', adjustment);
+});
