efbe1f3d365ae6f312339c1eea580afabdf43e29
David Wessman <david.wesmn@gmail.com>
2017-10-12 19:08:07 +0200

    Agenda enum - remove constants everywhere

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -2,7 +2,7 @@ class AgendasController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @agendas = Agenda.index.where(parent_id: nil).includes(:children)
+    @agendas = Agenda.by_index.where(parent_id: nil).includes(:children)
   end
 
   def show
--- app/view_objects/start_page.rb
+++ app/view_objects/start_page.rb
@@ -2,7 +2,7 @@ class StartPage
   attr_accessor :agendas, :news
 
   def initialize
-    @agendas = Agenda.index.where(parent_id: nil).includes(:children) || []
+    @agendas = Agenda.by_index.where(parent_id: nil).includes(:children) || []
     @news = News.order(created_at: :desc).limit(5).includes(:user) || []
   end
 end
