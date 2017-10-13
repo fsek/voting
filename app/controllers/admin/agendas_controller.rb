ba5c4cb2a7886ac4bc308a3887a9742b51b5f2a6
David Wessman <david.wesmn@gmail.com>
2017-10-13 17:09:35 +0200

    Agenda: replace wice_grid

--- app/controllers/admin/agendas_controller.rb
+++ app/controllers/admin/agendas_controller.rb
@@ -1,72 +1,77 @@
-class Admin::AgendasController < Admin::BaseController
-  load_and_authorize_resource
+# frozen_string_literal: true
 
-  def index
-    @agenda_grid = initialize_grid(Agenda, include: :parent,
-                                           order: 'sort_index')
-  end
+module Admin
+  # Handles creating and updating the agenda
+  class AgendasController < Admin::BaseController
+    authorize_resource
 
-  def new
-    @agenda = Agenda.new
-  end
-
-  def create
-    @agenda = Agenda.new(agenda_params)
-
-    if @agenda.save
-      expire_fragment('agenda_startpage')
-      redirect_to new_admin_agenda_path, notice: alert_create(Agenda)
-    else
-      render :new, status: 422
-    end
-  end
-
-  def edit
-    @agenda = Agenda.find(params[:id])
-  end
-
-  def update
-    @agenda = Agenda.find(params[:id])
-
-    if @agenda.update(agenda_params)
-      expire_fragment('agenda_startpage')
-      redirect_to edit_admin_agenda_path(@agenda), notice: alert_update(Agenda)
-    else
-      render :edit, status: 422
-    end
-  end
-
-  def destroy
-    @agenda = Agenda.find(params[:id])
-
-    if @agenda.destroy
-      expire_fragment('agenda_startpage')
-      flash[:notice] = t('agenda.deleted_ok')
-    else
-      flash[:alert] = @agenda.errors. full_messages_for(:destroy).to_sentence
+    def index
+      @agendas = Agenda.order(:sort_index).includes(:parent).page(params[:page])
     end
 
-    redirect_to admin_agendas_path
-  end
+    def new
+      @agenda = Agenda.new
+    end
 
-  def set_current
-    @agenda = Agenda.find(params[:id])
-    expire_fragment('agenda_startpage')
-    @success = @agenda.update(status: :current)
-    render
-  end
+    def create
+      @agenda = Agenda.new(agenda_params)
 
-  def set_closed
-    @agenda = Agenda.find(params[:id])
-    expire_fragment('agenda_startpage')
-    @success = @agenda.update(status: :closed)
-    render
-  end
+      if @agenda.save
+        expire_fragment('agenda_startpage')
+        redirect_to new_admin_agenda_path, notice: alert_create(Agenda)
+      else
+        render :new, status: 422
+      end
+    end
 
-  private
+    def edit
+      @agenda = Agenda.find(params[:id])
+    end
 
-  def agenda_params
-    params.require(:agenda).permit(:title, :index, :parent_id, :status,
-                                   :short, :description)
+    def update
+      @agenda = Agenda.find(params[:id])
+
+      if @agenda.update(agenda_params)
+        expire_fragment('agenda_startpage')
+        redirect_to edit_admin_agenda_path(@agenda),
+                    notice: alert_update(Agenda)
+      else
+        render :edit, status: 422
+      end
+    end
+
+    def destroy
+      @agenda = Agenda.find(params[:id])
+
+      if @agenda.destroy
+        expire_fragment('agenda_startpage')
+        flash[:notice] = t('agenda.deleted_ok')
+      else
+        flash[:alert] = @agenda.errors. full_messages_for(:destroy).to_sentence
+      end
+
+      redirect_to admin_agendas_path
+    end
+
+    def set_current
+      @agenda = Agenda.find(params[:id])
+      expire_fragment('agenda_startpage')
+      @success = @agenda.update(status: :current)
+      render
+    end
+
+    def set_closed
+      @agenda = Agenda.find(params[:id])
+      expire_fragment('agenda_startpage')
+      @success = @agenda.update(status: :closed)
+      render
+    end
+
+    private
+
+    def agenda_params
+      params.require(:agenda).permit(:title, :index, :parent_id,
+                                     :status, :short, :description)
+    end
   end
 end
