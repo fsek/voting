94847cd412837b4ee6e5903e5482e1826c9fcd3a
David Wessman <david.wesmn@gmail.com>
2017-11-16 16:27:22 +0100

    Adjustments: allow viewing index page again

---
+++ spec/requests/admin/quick_adjustments_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe("Allows quick adjustment of users", type: :request) do
+  let(:adjuster) { create(:user, role: :adjuster) }
+
+  it 'view quick adjustment page and find a user' do
+    sign_in(adjuster)
+    create(:user, presence: false, card_number: '1234-1234-1234-1234')
+    attributes = { vote_user: { card_number: '1234-1234-1234-1234' } }
+
+    get(admin_adjustments_path)
+    expect(response).to have_http_status(200)
+
+    post(search_admin_vote_users_path, params: attributes, xhr: true)
+    expect(response).to have_http_status(200)
+
+    non_existing = { vote_user: { card_number: '2345-2345-2345-2345' } }
+    post(search_admin_vote_users_path, params: non_existing, xhr: true)
+    expect(response).to have_http_status(200)
+  end
+end
