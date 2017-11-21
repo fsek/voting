5226b589707eab2c0d4e94df68f1b9f06225a781
David Wessman <david.wesmn@gmail.com>
2017-11-21 11:17:47 +0100

    Permissions for search: adds specs for checking

--- spec/requests/admin/search_users_spec.rb
+++ spec/requests/admin/search_users_spec.rb
@@ -5,13 +5,36 @@ require 'rails_helper'
 RSpec.describe('Search for users', type: :request) do
   let(:adjuster) { create(:user, role: :adjuster) }
 
-  it 'searches by first name' do
+  it 'finds a user' do
     sign_in(adjuster)
-    create(:user, firstname: 'Hilbert', lastname: 'Älg')
+    create(:user, firstname: 'Hilbert1', lastname: 'Älg')
+    create(:user, firstname: 'Hilbert2', lastname: 'Älg')
 
-    search_params = { search: { firstname: 'Hilbe' } }
+    search_params = { search: { firstname: 'Hilbert1' } }
 
     post(user_admin_search_path, params: search_params, xhr: true)
-    expect(response).to have_http_status(200)
+    expect(response.body).to include('Hilbert1')
+  end
+
+  it 'does not find any users' do
+    sign_in(adjuster)
+    create(:user, firstname: 'Hilbert1', lastname: 'Älg')
+    create(:user, firstname: 'Hilbert2', lastname: 'Älg')
+
+    search_params = { search: { firstname: 'Klas' } }
+
+    post(user_admin_search_path, params: search_params, xhr: true)
+    expect(response.body).to include(t('admin.searches.no_results'))
+  end
+
+  it 'finds all users if empty' do
+    sign_in(adjuster)
+    create(:user, firstname: 'Hilbert1', lastname: 'Älg')
+    create(:user, firstname: 'Hilbert2', lastname: 'Älg')
+
+    search_params = { search: { firstname: '' } }
+
+    post(user_admin_search_path, params: search_params, xhr: true)
+    expect(response.body).to include('Hilbert1', 'Hilbert2')
   end
 end
