c8dc5489fbd093952d2bb0168e63e6095e446aa3
David Wessman <david.wesmn@gmail.com>
2018-02-02 11:06:15 +0100

    Refactor static pages and its specs

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe('Static pages', type: :request) do
+  it 'renders about' do
+    get(about_path)
+    expect(response).to have_http_status(200)
+  end
+
+  it 'renders cookies' do
+    get(cookies_path)
+    expect(response).to have_http_status(200)
+  end
+
+  it 'renders tems' do
+    get(terms_path)
+    expect(response).to have_http_status(200)
+  end
+
+  it 'renders start page' do
+    create_list(:sub_item, 5)
+    create_list(:news, 5)
+    get(root_path)
+    expect(response).to have_http_status(200)
+  end
+end
