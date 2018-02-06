55fd372bb514a9c94f9ca2227273fd7f3cfe7107
David Wessman <david.wesmn@gmail.com>
2018-02-06 14:02:43 +0100

    Remove shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,6 @@ require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Do preferebly add it to 'spec/support/*'
-require 'shoulda/matchers'
 require 'devise'
 
 ActiveRecord::Migration.maintain_test_schema!
@@ -48,11 +47,4 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros
   config.include RequestMacro, type: :request
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
