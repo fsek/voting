1e3d13fb299876fc0230d6f3b8ebda58433b8b8a
David Wessman <david.wesmn@gmail.com>
2018-02-08 11:36:25 +0100

    Assure blank votes are supported

--- spec/system/votes_spec.rb
+++ spec/system/votes_spec.rb
@@ -24,4 +24,25 @@ RSpec.describe 'votes' do
     expect(page).to have_css('div.alert')
     expect(find('div.alert').text).to include(t('vote_posts.create.success'))
   end
+
+  it 'manages to create new blank vote_post', js: true do
+    sub_item = create(:sub_item, status: :current)
+    vote = create(:vote, :with_options, sub_item: sub_item)
+    user = create(:user, presence: true, votecode: 'abcd123')
+    vote.update!(status: :open)
+    LoginPage.new.visit_page.login(user)
+
+    page.visit(votes_path)
+
+    first(:linkhref, vote_vote_posts_path(vote)).click
+
+    fill_in('vote_post[votecode]', with: 'abcd123')
+
+    accept_confirm do
+      find('#vote_post_submit').click
+    end
+
+    expect(page).to have_css('div.alert')
+    expect(find('div.alert').text).to include(t('vote_posts.create.success'))
+  end
 end
