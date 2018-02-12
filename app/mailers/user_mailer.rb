d38386c7bedc69f04f06ab8846ec129af269c8b7
David Wessman <david.wesmn@gmail.com>
2018-02-12 20:41:20 +0100

    Mailer: setup layout

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,18 +4,19 @@
 class UserMailer < Devise::Mailer
   default(template_path: 'devise/mailer')
   # gives access to all helpers defined within `application_helper`.
-  helper :application
+  helper(EmailHelper)
+  layout('email')
 
   def confirmation_instructions(record, token, opts = {})
     set_message_id
-    opts[:subject] = t('users.mailer.subject_confirm', url: PUBLIC_URL)
+    opts[:subject] = t('users_mailer.subject_confirm', title: t('general.title'))
     @user = record
     super
   end
 
   def reset_password_instructions(record, token, opts = {})
     set_message_id
-    opts[:subject] = t('users.mailer.subject_password', url: PUBLIC_URL)
+    opts[:subject] = t('users_mailer.subject_password', title: t('general.title'))
     @user = record
     super
   end
