4b12d8b8f2664050c0cf992e0d8e8bafca8a0d7b
David Wessman <david.wesmn@gmail.com>
2018-06-15 07:39:16 +0200

    Reset: set votecodes to nil

--- spec/services/cleanup_spec.rb
+++ spec/services/cleanup_spec.rb
@@ -6,15 +6,18 @@ Rails.application.load_tasks
 
 RSpec.describe 'Cleanup task' do
   it 'removes everything but users' do
+    create_list(:user, 5, :votecode)
     create_list(:vote, 5)
     create_list(:adjustment, 5)
-    expect(Audit.count). to eq(10)
-    expect(Adjustment.with_deleted.count). to eq(5)
-    expect(Vote.with_deleted.count). to eq(5)
-    expect(Item.with_deleted.count). to eq(10)
+    expect(User.pluck(:votecode).uniq).not_to eq([nil])
+    expect(Audit.count). to eq(15)
+    expect(Adjustment.with_deleted.count).to eq(5)
+    expect(Vote.with_deleted.count).to eq(5)
+    expect(Item.with_deleted.count).to eq(10)
 
     Rake::Task['cleanup:keep_users'].invoke
 
+    expect(User.pluck(:votecode).uniq).to eq([nil])
     expect(Audit.count). to eq(0)
     expect(Adjustment.with_deleted.count). to eq(0)
     expect(Vote.with_deleted.count). to eq(0)
